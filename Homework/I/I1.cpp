3fe181a9ace286b277ebe33eff61eff119807af9
Berezin-Leonid <Berezinlion@gmail.com>
2024-04-18 19:50:15 +0300

    Partial Solution

--- Homework/I/I1.cpp
+++ Homework/I/I1.cpp
@@ -16,14 +16,25 @@ int function(std::string const & s1, std::string const & s2)
     T heap;
     for (int i = 0; i <= len1; ++i) {
         for (int j = 0; j <= len2; ++j) {
-            for(int k = 0; k < len1 - i; ++k) {
-                for(int m = 0; m < len1 - i; ++m) {
-                    if (std::find(heap.begin(), heap.end(), s1.substr(k, k + i) + 
-                            s2.substr(m, m + j)) == heap.end()) {
+            for(int k = 0; k <= len1 - i; ++k) {
+                for(int m = 0; m <= len1 - i; ++m) {
+                    //std::string s = s1.substr(k, i) + s2.substr(m,j);
+                    //std::cout << s << std::endl;
+                    if (std::find(heap.begin(), heap.end(), s1.substr(k, i) + 
+                            s2.substr(m, j)) == heap.end()) {
 
                         heap.push_back(
-                            s1.substr(k, k + i) + 
-                            s2.substr(m, m + j));
+                            s1.substr(k, i) + 
+                            s2.substr(m, j));
+
+                    }
+
+                    if (std::find(heap.begin(), heap.end(), s2.substr(m, j) +
+                            s1.substr(k, i)) == heap.end()) {
+
+                        heap.push_back(
+                            s2.substr(m, j) +
+                            s1.substr(k, i)); 
 
                     }
                 }
@@ -34,8 +45,9 @@ int function(std::string const & s1, std::string const & s2)
     return heap.size();
 }
 
+/*
 template <>
-int function<std::set<std::string>>(std::string const & s1, std::string const & s2)
+int function<std::set<std::string> >(std::string const & s1, std::string const & s2)
 {
     int len1 = s1.length();
     int len2 = s2.length();
@@ -47,12 +59,12 @@ int function<std::set<std::string>>(std::string const & s1, std::string const & 
         for (int j = 1; j <= len2; ++j) {
             for(int k = 0; k < len1 - i; ++k) {
                 for(int m = 0; m < len1 - j; ++m) {
-                    if (std::find(heap.begin(), heap.end(), s1.substr(k, k + i) + 
-                            s2.substr(m, m + j)) == heap.end()) {
+                    if (std::find(heap.begin(), heap.end(), s1.substr(k, i) + 
+                            s2.substr(m, j)) == heap.end()) {
 
                         heap.insert(
-                            s1.substr(k, k + i) + 
-                            s2.substr(m, m + j));
+                            s1.substr(k, i) + 
+                            s2.substr(m, j));
 
                     }
                 }
@@ -62,15 +74,27 @@ int function<std::set<std::string>>(std::string const & s1, std::string const & 
 
     return heap.size();
 }
+*/
 
 namespace with_vector {
-    int fun(std::string const &, std::string const &);
+    int fun(std::string const & s1, std::string const & s2)
+    {
+        return function<std::vector<std::string> >(s1,s2);
+    }
 }
 
-int
-with_vector::fun(std::string const &s1, std::string const &s2)
-{
-    return function<std::vector<std::string>>(s1,s2);
+namespace with_list {
+    int fun(std::string const & s1, std::string const & s2)
+    {
+        return function<std::list<std::string> >(s1,s2);
+    }
+}
+
+namespace with_best {
+    int fun(std::string const & s1, std::string const & s2)
+    {
+        return function<std::list<std::string> >(s1,s2);
+    }
 }
 
 
@@ -78,7 +102,7 @@ with_vector::fun(std::string const &s1, std::string const &s2)
 int
 main()
 {
-    std::cout << with_vector::fun("ab","12") << std::endl;
+    std::cout << with_best::fun("ab","12") << std::endl;
     return 0;
 }
 #endif
