0ba9b63eb6d276c6ec0787ff84a51413c3e03e76
Berezin-Leonid <Berezinlion@gmail.com>
2024-04-18 23:42:06 +0300

    У Вари зачетный шпагат

---
+++ Homework/I/I3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <exception>
+#include <vector>
+
+
+template <typename T>
+typename T::value_type last3(T const & obj)
+{
+    if (obj.size() < 3) {
+        throw std::exception();
+    }
+
+    typename T::const_iterator it = obj.end();
+
+    --it;
+    --it;
+    --it;
+
+    return *it++ + *it++ + *it++;
+}
+
+
+#ifdef LEO
+int
+main()
+{
+    std::vector<int> v;
+    v.push_back(1); 
+    v.push_back(2); 
+    v.push_back(3); 
+    v.push_back(4); 
+    std::cout << last3(v) << std::endl;
+    return 0;
+}
+#endif
